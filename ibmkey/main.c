3e4159746bcd767d4672a5fe5b7f675702e6114c
Chris Lee <clee@mg8.org>
2008-06-22 21:16:28 -0700

    Remove unused B8 macro

--- ibmkey/main.c
+++ ibmkey/main.c
@@ -63,20 +63,6 @@
  *                GND   GND
  */
 
-#define HEX__(n) 0x##n##LU
-
-/* 8-bit conversion function */
-#define B8__(x) ((x&0x0000000FLU)?1:0) \
-+((x&0x000000F0LU)?2:0) \
-+((x&0x00000F00LU)?4:0) \
-+((x&0x0000F000LU)?8:0) \
-+((x&0x000F0000LU)?16:0) \
-+((x&0x00F00000LU)?32:0) \
-+((x&0x0F000000LU)?64:0) \
-+((x&0xF0000000LU)?128:0)
-
-#define B8(d) ((unsigned char)B8__(HEX__(d)))
-
 /* The LED states */
 #define LED_NUM     0x01
 #define LED_CAPS    0x02
